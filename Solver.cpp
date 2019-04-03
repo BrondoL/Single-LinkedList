14891a1b95f0f35ea5b9fd61528a0b3271cc379d
arianza1210 <48575505+arianza1210@users.noreply.github.com>
2019-04-03 10:17:46 +0700

    Add files via upload
    update file

--- Solver.cpp
+++ Solver.cpp
@@ -2,9 +2,63 @@
 
 using namespace std;
 
+struct TNode{
+    int data;
+    TNode *next;
+};TNode *head, *tail,*depan,*belakang;
+void init(){
+    head=NULL;
+    tail=NULL;
+    depan=NULL;
+    belakang=NULL;
+}
+
+int isEmpty(){
+    if(tail==NULL){
+    return 1;
+    }else{
+        return 0;
+    }
+}
+void insertDepan(int databaru)
+{
+    TNode *baru;
+    baru =new TNode;
+    baru->data=databaru;
+    baru->next=NULL;
+    if(isEmpty()==1)
+    {
+        head=tail=baru;
+        tail->next=NULL;
+    }else{
+        baru->next=head;
+        head=baru;
+    }
+    cout<<"Input berhasil";
+}
+
+void insertBelakang(int databaru){
+    TNode *baru;
+    baru=new TNode;
+    baru->data=databaru;
+    baru->next=NULL;
+    if(isEmpty()==1){
+        head=baru;
+        tail=baru;
+        tail->next=NULL;
+    }else
+    {
+        tail->next=baru;
+        tail=baru;
+    }
+    cout<<"\n Input berhasil"<<endl;
+}
+
+
 int main()
 {
+	
 
-	cin.get()
+	cin.get();
 	return 0;
 }
